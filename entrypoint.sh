cbebd49951fcb2fe72eaa7c5565d08c9be6c90cc
Bryan Schuetz <bschuetz@gmail.com>
2020-07-01 12:52:40 -0400

    More Better

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 echo '👍 ENTRYPOINT HAS STARTED—INSTALLING THE GEM BUNDLE'
 bundle install
+echo '👍 Building The Site'
+bundle exec jekyll build
+echo '👍 Proofing the Site'
 bundle exec htmlproofer build
 echo '👍 GREAT SUCCESS!'
